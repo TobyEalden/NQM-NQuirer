4f7029fe2a1be81043f2e6f8d8f9d2d5ef95cdb3
toby ealden <toby.ealden@gmail.com>
2016-10-09 20:41:03 +0100

    app constants and utilities

---
+++ client/imports/app-utils.js
@@ -0,0 +1,22 @@
+class AppUtils {
+  static constants = {
+    ui: {
+      wideModeBreakPoint: 1024,
+      mediumModeBreakPoint: 525,
+      narrowModeBreakPoint: 320,
+      titleBarHeight: 50,
+      verticalButtonPanelWidth: 50,
+      floatingNavWidth: 300,
+      dockedNavWidth: 300,
+      widgetTitleBarHeight: 18
+    },
+    query: {
+      viewMode: "vm",
+      dockedSideBarOpen: "sbdo",
+      floatingSideBarOpen: "sbfo",
+      sideBarView: "sbv"
+    }
+  }
+}
+
+export default AppUtils;
